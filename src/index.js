0c34df7a5d1867698a2315cba598e360f884ddcf
Siarhei Prymakovich <prima.psa@gmail.com>
2021-03-15 22:51:07 +0300

    feat: implement reverse-int fnc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let string = Math.abs(n);
+    string = String(string);
+    string = string.split('').reverse().join('');
+    string = Number(string);
+    return string;  
 }
